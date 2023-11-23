5424520f2f2f985ee4e806323db4083f86d21a95
likangle <likangle@corp.netease.com>
2023-11-23 10:11:05 +0800

    chore: tip

--- src/popup/footer.tsx
+++ src/popup/footer.tsx
@@ -1,6 +1,7 @@
 import { optionsURL } from '@/common/contants'
 import Link from '@/components/Link'
 import { ChromeOutlined, GithubOutlined, SettingOutlined } from '@ant-design/icons'
+import { Tooltip } from 'antd'
 
 import { version } from '../../package.json'
 
@@ -8,12 +9,16 @@ function Footer() {
     return (
         <div className="dark:bg-[#24292f] dark:text-[#818995] bg-[#f2f5fa] text-[16px] text-c60 fixed bottom-0 left-0 px-12 w-[100%] h-38 flex justify-between items-center">
             <div className="flex items-center gap-8">
-                <Link target="_blank" href="https://baidu.com">
-                    <ChromeOutlined />
-                </Link>
-                <Link target="_blank" href="https://baidu.com">
-                    <GithubOutlined />
-                </Link>
+                <Tooltip title="敬请期待" placement="right">
+                    <Link target="_blank" href="javascript:">
+                        <ChromeOutlined />
+                    </Link>
+                </Tooltip>
+                <Tooltip title="敬请期待" placement="right">
+                    <Link target="_blank" href="javascript:">
+                        <GithubOutlined />
+                    </Link>
+                </Tooltip>
             </div>
             <div className="flex items-center gap-8">
                 <Link target="_blank" href={optionsURL}>
