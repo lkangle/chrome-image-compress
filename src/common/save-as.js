deca310103bc030922a7f61bd97cf9e2ecf7adc4
likangle <likangle@corp.netease.com>
2024-06-06 16:08:25 +0800

    fix: 替换的字符

--- src/common/save-as.js
+++ src/common/save-as.js
@@ -1,7 +1,8 @@
 import { saveAs as save_as } from 'file-saver'
 
 export const saveAs = (blob, filename) => {
-    // Remove the @1x, @2x, @3x from the filename
-    const name = filename.replace(/@[1-3]x\.(\w+\.)?/, '.')
+    const name = filename.replace(/@([1-3])x\.(\w+\.)?/, (_, $0) => {
+        return `-${$0}x.`
+    })
     save_as(blob, name)
 }
